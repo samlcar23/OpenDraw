81e860418e23eb7920fc741b3e71c5d280c8c2e8
vassalty <vassalty@mail.gvsu.edu>
2017-11-27 08:59:44 -0500

    added temporary client gui

---
+++ src/WhiteboardGUI.java
@@ -0,0 +1,240 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JColorChooser;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class WhiteboardGUI extends JComponent {
+
+	private Graphics2D graphics2D;
+
+	private Image image;
+
+	private int clickX, clickY, dragX, dragY;
+
+	private Color color;
+
+	private String shape;
+
+	private int scale;
+
+	private int SCALEMIN = 10;
+
+	private int SCALEMAX = 100;
+
+
+	public Graphics2D getGraphics() {
+		return graphics2D;
+	}
+
+	public void setGraphics(Graphics2D graphicInput) {
+		graphics2D = graphicInput;
+	}
+
+	public int[] getClicked() {
+		return new int[] { clickX, clickY };
+	}
+
+	public int[] getDrag() {
+		return new int[] { dragX, dragY };
+	}
+
+	public WhiteboardGUI() {
+
+		color = Color.black;
+
+		shape = "line";
+		
+		scale = 10;
+
+		this.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				clickX = e.getX();
+				clickY = e.getY();
+
+				dragX = e.getX();
+				dragY = e.getY();
+
+				if (graphics2D != null) {
+					graphics2D.setColor(color);
+					draw(clickX, clickY, dragX, dragY, shape);
+				}
+
+				repaint();
+			}
+		});
+
+		this.addMouseMotionListener(new MouseMotionAdapter() {
+			public void mouseDragged(MouseEvent e) {
+				dragX = e.getX();
+				dragY = e.getY();
+
+				if (graphics2D != null) {
+					graphics2D.setColor(color);
+					draw(clickX, clickY, dragX, dragY, shape);
+				}
+
+				repaint();
+
+				clickX = dragX;
+				clickY = dragY;
+			}
+		});
+
+		JFrame frame = new JFrame();
+
+		JPanel buttonPanel = new JPanel();
+
+		JButton clearButton = new JButton("Clear");
+		JButton colorButton = new JButton("Color");
+
+		JButton circleButton = new JButton("Circle");
+		JButton squareButton = new JButton("Square");
+		JButton lineButton = new JButton("Line");
+		JButton stampButton = new JButton("Stamp");
+
+		JSlider scaleSlider = new JSlider();
+
+		JCheckBox filledBox = new JCheckBox();
+
+		this.setPreferredSize(frame.getPreferredSize());
+		
+		scaleSlider.setValue(10);
+		scaleSlider.setMaximum(SCALEMAX);
+		scaleSlider.setMinimum(SCALEMIN);
+		scaleSlider.setSnapToTicks(true);
+		scaleSlider.setPaintTicks(true);
+		scaleSlider.setMajorTickSpacing(10);
+
+		buttonPanel.setLayout(new GridLayout(2, 2));
+
+		buttonPanel.add(clearButton);
+		buttonPanel.add(colorButton);
+		buttonPanel.add(circleButton);
+		buttonPanel.add(squareButton);
+		buttonPanel.add(lineButton);
+		buttonPanel.add(stampButton);
+		buttonPanel.add(new JLabel("Scale:"));
+		buttonPanel.add(scaleSlider);
+
+		frame.add(this, BorderLayout.CENTER);
+		frame.add(buttonPanel, BorderLayout.SOUTH);
+
+		frame.setSize(1000, 500);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+		clearButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				graphics2D.setPaint(Color.white);
+				graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+				graphics2D.setPaint(Color.black);
+				repaint();
+			}
+		});
+
+		colorButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				color = JColorChooser.showDialog(frame, "Choose a color", color);
+				graphics2D.setColor(color);
+				repaint();
+			}
+		});
+
+		circleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "circle";
+				repaint();
+			}
+		});
+
+		squareButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "square";
+				repaint();
+			}
+		});
+
+		lineButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "line";
+				repaint();
+			}
+		});
+
+		stampButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = (String) JOptionPane.showInputDialog(frame, "Chose a word to stamp:", "Stamp Tool",
+						JOptionPane.PLAIN_MESSAGE);
+				if (shape.equals(null) == true) {
+					shape = "line";
+				}
+				
+				repaint();
+			}
+		});
+
+		//
+
+		scaleSlider.addChangeListener(new ChangeListener() {
+			public void stateChanged(ChangeEvent e) {
+				scale = scaleSlider.getValue();
+			}
+		});
+
+	}
+
+	public void paintComponent(Graphics g) {
+		if (image == null) {
+			image = createImage(this.getSize().width, this.getSize().height);
+			graphics2D = (Graphics2D) image.getGraphics();
+
+			graphics2D.setPaint(Color.white);
+			graphics2D.fillRect(0, 0, getSize().width, this.getSize().height);
+
+			graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+			graphics2D.setPaint(Color.black);
+
+			repaint();
+		}
+
+		g.drawImage(image, 0, 0, null);
+	}
+
+	public void draw(int oX, int oY, int cX, int cY, String shape) {
+		if (shape.equals("line") == true) {
+			graphics2D.drawLine(oX, oY, cX, cY);
+		} else if (shape.equals("circle") == true) {
+			graphics2D.fillOval(cX, cY, scale, scale);
+		} else if (shape.equals("square") == true) {
+			graphics2D.fillRect(cX, cY, scale, scale);
+		} else if (shape.equals(null) == false) {
+			graphics2D.setFont(new Font("TimesRoman", Font.PLAIN, scale));
+			graphics2D.drawString(shape, cX, cY);
+		}
+	}
+
+	public static void main(String[] args) {
+		WhiteboardGUI whiteboard = new WhiteboardGUI();
+	}
+}
