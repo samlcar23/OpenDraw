9084ea322bf9f6ee1b6a5fe1d30372411d689a73
vassalty <vassalty@mail.gvsu.edu>
2017-12-01 11:12:35 -0500

    gui update
    gui look change
    added/removed buttons

--- src/Whiteboard.java
+++ src/Whiteboard.java
@@ -7,7 +7,11 @@ import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionAdapter;
-
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JColorChooser;
@@ -19,60 +23,68 @@ import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public class Whiteboard {
-	
+public class Whiteboard extends Observable {
+
 	private Graphics2D graphics2D;
-	
+
 	private Color color;
-	
+
 	private String shape;
-	
+
 	private DrawSpace drawSpace;
-	
+
 	private int scale;
-	
-	private int clickX, clickY, dragX, dragY, prevX, prevY;
-	
+
+	private int clickX, clickY, dragX, dragY;
+
 	private boolean isFilled;
-	
+
 	private int SCALEMIN = 10;
-			
+
 	private int SCALEMAX = 100;
-	
 
 	public Whiteboard() {
-		
+
 		isFilled = true;
 
 		color = Color.black;
 
-		shape = "line";
-		
+		shape = "pen";
+
 		scale = 10;
-		
+
 		drawSpace = new DrawSpace();
-		
+
 		graphics2D = drawSpace.getGraphics();
 
 		JFrame frame = new JFrame();
 
 		JPanel buttonPanel = new JPanel();
-		
+
 		JPanel bottomPanel = new JPanel();
+		JPanel bottomPanelWest = new JPanel();
+		JPanel bottomPanelEast = new JPanel();
 
 		JButton clearButton = new JButton("Clear");
-		
+
+		JButton saveButton = new JButton("Save");
+
+		JButton exitButton = new JButton("Exit");
+
 		JButton colorButton = new JButton("Color");
 
 		JButton circleButton = new JButton("Circle");
 		JButton squareButton = new JButton("Square");
-		JButton lineButton = new JButton("Line");
+		JButton penButton = new JButton("Pen");
+		JButton eraserButton = new JButton("Eraser");
+		JButton styleButton = new JButton("Style");
+		JButton blankButton = new JButton("blank");
 		JButton stampButton = new JButton("Stamp");
 
 		JSlider scaleSlider = new JSlider();
 
 		JCheckBox filledBox = new JCheckBox();
-		
+
 		scaleSlider.setValue(10);
 		scaleSlider.setMaximum(SCALEMAX);
 		scaleSlider.setMinimum(SCALEMIN);
@@ -80,30 +92,49 @@ public class Whiteboard {
 		scaleSlider.setPaintTicks(true);
 		scaleSlider.setMajorTickSpacing(10);
 
-		buttonPanel.setLayout(new GridLayout(9, 1));
-		
-		filledBox.setSelected(true);
-		
-		bottomPanel.add(clearButton);
+		GridLayout layout = new GridLayout(4, 2);
 
-		buttonPanel.add(colorButton);
+		layout.setHgap(10);
+		layout.setVgap(10);
+
+		buttonPanel.setLayout(layout);
+		bottomPanel.setLayout(new BorderLayout());
+
+		filledBox.setSelected(true);
+
+		bottomPanelWest.add(clearButton);
+		bottomPanelWest.add(saveButton);
+		bottomPanelWest.add(exitButton);
+
+		bottomPanelEast.add(new JLabel("Scale:"));
+		bottomPanelEast.add(scaleSlider);
+
+		bottomPanelEast.add(new JLabel("Fill:"));
+		bottomPanelEast.add(filledBox);
+
+		buttonPanel.add(penButton);
+		buttonPanel.add(styleButton);
+
 		buttonPanel.add(circleButton);
 		buttonPanel.add(squareButton);
-		buttonPanel.add(lineButton);
+
+		buttonPanel.add(blankButton);
 		buttonPanel.add(stampButton);
-		buttonPanel.add(new JLabel("Scale:"));
-		buttonPanel.add(scaleSlider);
-		buttonPanel.add(new JLabel("Fill:"));
-		buttonPanel.add(filledBox);
+
+		buttonPanel.add(colorButton);
+		buttonPanel.add(eraserButton);
+
+		bottomPanel.add(bottomPanelWest, BorderLayout.LINE_START);
+		bottomPanel.add(bottomPanelEast, BorderLayout.LINE_END);
 
 		frame.add(drawSpace, BorderLayout.CENTER);
 		frame.add(bottomPanel, BorderLayout.SOUTH);
-		frame.add(buttonPanel, BorderLayout.EAST);
+		frame.add(buttonPanel, BorderLayout.AFTER_LINE_ENDS);
 
 		frame.setSize(1000, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
-		
+
 		drawSpace.addMouseListener(new MouseAdapter() {
 			public void mousePressed(MouseEvent e) {
 				clickX = e.getX();
@@ -117,7 +148,7 @@ public class Whiteboard {
 				}
 
 				update();
-				
+
 			}
 		});
 
@@ -131,43 +162,62 @@ public class Whiteboard {
 				}
 
 				update();
-				
+
 				clickX = e.getX();
 				clickY = e.getY();
 			}
 		});
-		
-		/*
-		 * BROKEN: mouse listener to show cursor as selected paint brush
-		 */
-		
-//		drawSpace.addMouseMotionListener(new MouseMotionAdapter() {
-//			public void mouseMoved(MouseEvent e) {
-//				int moveX = e.getX();
-//				int moveY = e.getY();
-//
-//				if (graphics2D != null) {
-//					drawSpace.getGraphics().setColor(Color.WHITE);
-//					drawSpace.draw(prevX, prevY, prevX, prevY, shape, scale, isFilled);
-//					
-//					drawSpace.getGraphics().setColor(color);
-//					drawSpace.draw(moveX, moveY, moveX, moveY, shape, scale, isFilled);
-//				}
-//
-//				update();
-//				
-//				prevX = moveX;
-//				prevY = moveY;
-//			}
-//		});
 
 		clearButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				graphics2D.setPaint(Color.white);
 				graphics2D.fillRect(0, 0, drawSpace.getSize().width, drawSpace.getSize().height);
 				graphics2D.setPaint(Color.black);
-				
+
 				drawSpace.update();
+			}
+		});
+
+		saveButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String name = (String) JOptionPane.showInputDialog(frame, "What would you like to name the file?",
+						"", JOptionPane.PLAIN_MESSAGE);
+				if (name != null) {
+					String filename = "./" + name + ".png";
+					
+					BufferedImage saveImage = new BufferedImage(drawSpace.getWidth(), drawSpace.getHeight(),
+							BufferedImage.TYPE_INT_RGB);
+					Graphics2D graphic = saveImage.createGraphics();
+					
+					drawSpace.paintAll(graphic);
+					
+					try {
+						if (ImageIO.write(saveImage, "png", new File(filename))) {
+							JOptionPane.showMessageDialog(frame,
+								    name + ".png saved!");
+						}
+					} catch (IOException exception) {
+						exception.printStackTrace();
+					}
+				}
+			}
+		});
+
+		exitButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+
+		penButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "pen";
+			}
+		});
+
+		styleButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "style";
 			}
 		});
 
@@ -189,10 +239,16 @@ public class Whiteboard {
 				shape = "square";
 			}
 		});
-
-		lineButton.addActionListener(new ActionListener() {
+		
+		blankButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				shape = "line";
+
+			}
+		});
+
+		eraserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "eraser";
 			}
 		});
 
@@ -201,8 +257,14 @@ public class Whiteboard {
 				shape = (String) JOptionPane.showInputDialog(frame, "Chose a word to stamp:", "Stamp Tool",
 						JOptionPane.PLAIN_MESSAGE);
 				if (shape.equals(null) == true) {
-					shape = "line";
+					shape = "pen";
 				}
+			}
+		});
+
+		eraserButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				shape = "eraser";
 			}
 		});
 
@@ -211,19 +273,19 @@ public class Whiteboard {
 				scale = scaleSlider.getValue();
 			}
 		});
-		
+
 		filledBox.addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent e) {
 				isFilled = filledBox.isSelected();
 			}
 		});
 	}
-	
+
 	public void update() {
 		drawSpace.update();
 		graphics2D = drawSpace.getGraphics();
 	}
-	
+
 	public static void main(String args[]) {
 		Whiteboard whiteboard = new Whiteboard();
 	}
